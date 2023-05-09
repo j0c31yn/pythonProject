120fa02dea2ce4625497d5047da709bf0c037ca9
jocelynchoi <jocelyn.choi@online.island.edu.hk>
2023-05-09 10:51:33 +0800

    Initial commit

--- Snowman.py
+++ Snowman.py
@@ -6,14 +6,14 @@ arcade.draw_rectangle_filled(400, 200, 1000, 500, arcade.color.LIGHT_BLUE)
 arcade.draw_rectangle_filled(400, 450, 1000, 500, arcade.color.DARK_BLUE)
 
 
-def snowman(x,y):
-    arcade.draw_circle_filled(250, 250, 60, arcade.color.WHITE)
-    arcade.draw_circle_filled(250, 330, 50, arcade.color.WHITE)
-    arcade.draw_circle_filled(250, 400, 40, arcade.color.WHITE)
-    arcade.draw_circle_filled(265, 410, 5, arcade.color.BLACK)
-    arcade.draw_circle_filled(235, 410, 5, arcade.color.BLACK)
-def main():
-    snowman(250,250)
+def snowman(x, y):
+    arcade.draw_circle_filled(x, y-150, 60, arcade.color.WHITE)
+    arcade.draw_circle_filled(x, y-70, 50, arcade.color.WHITE)
+    arcade.draw_circle_filled(x, y, 40, arcade.color.WHITE)
+    arcade.draw_circle_filled(x+15, y+10, 5, arcade.color.BLACK)
+    arcade.draw_circle_filled(x-15, y+10, 5, arcade.color.BLACK)
+
+snowman(250,250)
 
 arcade.finish_render()
 arcade.run()
