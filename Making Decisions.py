51a51e1591f1d8f3389514cf0324ade8e9ed8479
jocelynchoi <jocelyn.choi@online.island.edu.hk>
2023-05-23 10:32:25 +0800

    Initial commit

---
+++ Making Decisions.py
@@ -0,0 +1,38 @@
+# 1
+x = 5
+if x == 10:
+   print("x is equal to 10")
+else:
+   print("x is not equal to 10")
+
+#2
+x = 5
+y = 10
+if x > y:
+    print("x is greater than y")
+else:
+    print("x is less than or equal to y")
+#3
+x = 1
+if x > 0:
+    print("x is positive")
+elif x < 0:
+   print("x is negative")
+else:
+   print("x is zero")
+
+#4
+x = 2
+y = 3
+if x == 10 and y == 5:
+   print("x is equal to 10 and y is equal to 5")
+else:
+   print("x and y are not equal to their respective values")
+
+#5
+x = 2
+y = 3
+if x > 10 or y < 5:
+   print("either x is greater than 10 or y is less than 5")
+else:
+   print("neither x is greater than 10 nor y is less than 5")
